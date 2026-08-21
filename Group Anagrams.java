2f39fffdaacfcb72503dea371884efc61c0c8baf
Monisha-codes26 <monimuthu217_bit27@mepcoeng.ac.in>
2026-08-21 12:11:07 +0530

    Create Group Anagrams.java

---
+++ Group Anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedKey = new String(chars);
+            if (!map.containsKey(sortedKey)) {
+                map.put(sortedKey, new ArrayList<>());
+            }
+            map.get(sortedKey).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
